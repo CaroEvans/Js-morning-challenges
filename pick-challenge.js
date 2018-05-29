a251bbb1e6fa56fcd2c4007bcde78af6ad54e3ec
Caro Evans <caroline.w.evans@gmail.com>
2018-05-29 11:16:57 +1000

    passed test

--- pick-challenge.js
+++ pick-challenge.js
@@ -5,9 +5,9 @@ Pick
 Return a copy of the object, filtered to only have values for the
 whitelisted keys.
 Example:
-pick({name: ‘moe’, age: 50, userid: ‘moe1’}, ‘name’, ‘age’);
-=> {name: ‘moe’, age: 50}
-Hint: Given there’s an indefinite number of arguments, we’ll need to loop
+pick({name: 'moe', age: 50, userid: 'moe1'}, 'name', 'age');
+=> {name: 'moe', age: 50}
+Hint: Given there's an indefinite number of arguments, we'll need to loop
 through them. Look up the “rest parameter” syntax.
 */
 
@@ -15,64 +15,36 @@ through them. Look up the “rest parameter” syntax.
 const _ = require('underscore')
 
 const pick_key = (list, ...args) => {
-    args.forEach(function(key_name) {
-      console.log(key_name)
+    args.forEach(function(key_names) {
+      console.log(key_names) // returns strings
+      // console.log(args) //returns as an array
     })
 }
 
 const pick = (list, ...args) => {
-  list.forEach(function(list_obj){
+  const new_obj = {}
     args.forEach(function(key_name){
-      var x = _.filter([list_obj], function(key){
-        return key[key_name]
-      })
-      console.log(x)
+      new_obj[key_name] = list[key_name]
     })
-  })
+  return new_obj
 }
+// var stooges = {name: 'moe', age: 40}
+// console.log(pick(stooges,'name'))
 
-//Underscore example
-  // var evens = _.filter([1, 2, 3, 4, 5, 6], function(num){
-  //     return num % 2 == 0;
-  //   })
-//
+//Check your solution by running these tests: mocha *this_filename*
+const assert = require('assert');
 
+describe('Pick', () => {
+ const list = {a: 1, b: 2, c: 3};
 
-var stooges = [{name: 'moe', age: 40},
-               {name: 'larry', age: 50},
-               {name: 'curly', age: 60}];
-pick(stooges,'name')
-
-
-//More Underscore functions
-
-// _.each({one: 1, two: 2, three: 3}, alert);
-// _.map([1, 2, 3], function(num){ console.log(num * 3) });
-//
-// var sum = _.reduce([1, 2, 3], function(memo, num){
-//   return memo + num;
-//   },
-// 0);
-// console.log(sum)
-
-
-// //Check your solution by running these tests: mocha *this_filename*
-// const assert = require(‘assert’);
-//
-// describe(‘Pick’, () => {
-//  const list = {a: 1, b: 2, c: 3};
-//
-//  it(‘can pick a single property’, () => {
-//    const result = pick(list, ‘a’);
-//    const expected = {a: 1};
-//    assert.deepEqual(result, expected);
-//  })
-//  it(‘can pick multiple properties’, () => {
-//      const result = pick(list, ‘a’, ‘c’);
-//      const expected = {a: 1, c: 3};
-//      assert.deepEqual(result, expected);
-//  })
-// });
-//
-// pick(list, ‘a’);
-// pick(list, ‘a’, ‘c’);
+ it('can pick a single property', () => {
+   const result = pick(list, 'a');
+   const expected = {a: 1};
+   assert.deepEqual(result, expected);
+ })
+ it('can pick multiple properties', () => {
+     const result = pick(list, 'a', 'c');
+     const expected = {a: 1, c: 3};
+     assert.deepEqual(result, expected);
+ })
+});
